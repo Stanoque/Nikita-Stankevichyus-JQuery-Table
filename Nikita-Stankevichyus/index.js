49bfd9dbbae940006ee8842f144c1104064c1544
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-17 19:50:33 +0300

    Basic functionality through jQuery and lodash is reached.

--- Nikita-Stankevichyus/index.js
+++ Nikita-Stankevichyus/index.js
@@ -1,28 +1,38 @@
 "use strict";
 
-const list = {};
+const LIST = [];
 
-function Good(name='', email='', count=0, price=0, places={}) {
+function Good(name='', email='', count=0, price=0, russia=[], belorus=[], usa=[]) {
 
   this.name = name;
   this.email = email;
   this.count = count;
   this.price = price;
 
-  this.delivery = places;
+  this.delivery = {
+    russia: russia,
+    belorus: belorus,
+    usa: usa,
+  }
 }
 
-let kek = new Good('kek', 'shek', 'cheburek', 0, {
-  russia: ['moscow', 'spb'],
-  belarus: ['hotlany'],
-});
+
+LIST.push(new Good('Just Good', 'someemail@gmail.com', 3, 222, [true], [], [true,,true]));
+LIST.push(new Good('G Good', 'someeail@gmail.com', 4, 22, [true], [], [true,,true]));
+LIST.push(new Good('B Good', 'smeemail@gmail.com', 5, 222, [], [], [true,,true]));
+LIST.push(new Good('A Good', 'somemail@gmail.com', 1, 222, [true], [], [,,true]));
 
 let buttonDelete = $(".button_delete");
 let buttonClose = $(".modal_close");
 
-$("#triangle").click(() => {
-  $("#triangle").toggleClass('flip');
+$("#triangle_name").click(() => {
+  $("#triangle_name").toggleClass('flip');
 });
+
+$("#triangle_price").click(() => {
+  $("#triangle_price").toggleClass('flip');
+});
+
 
 
 $("#delete_1").click(() => {
@@ -44,4 +54,72 @@ $(".modal_close").click(() => {
 $(".modal_cancel").click(() => {
   $(".modal_fade").removeClass("modal_fade_trick");
   $(".my_modal_edit").css("display", "none");
+});
+
+
+let rowTemp = _.template($("#row_template").html());
+
+
+
+
+$("#delete_4").click(() => {
+  $(".modal_fade").addClass("modal_fade_trick");
+  $(".my_modal_delete").css("display", "block");
+});
+
+$("#edit_4").click(() => {
+  $(".modal_fade").addClass("modal_fade_trick");
+  $(".my_modal_edit").css("display", "block");
+});
+
+LIST.forEach((good, number) => {
+  $(rowTemp({
+    name: good.name,
+    price: good.price,
+    count: good.count,
+    idEdit: 'edit_'+number,
+    idDelete: 'delete_'+number,
+  })).appendTo('#table_body');
+
+  let modalTemp = _.template($("#modal_edit_template").html());
+
+
+
+  $(modalTemp({
+    modalId: 'modal_edit_'+number,
+    name: good.name,
+    price: good.price,
+    count: good.count,
+    email: good.email,
+    city_1: 'FIXME',
+    city_2: 'FIXME',
+    city_3: 'FIXME',
+    saveId: 'modal_save_'+number,
+    cancelId: 'modal_cancel_'+number,
+  })).appendTo('.main');
+
+  $('#edit_'+number).click(() => {
+    $(".modal_fade").addClass("modal_fade_trick");
+    $("#modal_edit_"+number).css("display", "block");
+  });
+
+  $("#modal_save_"+number).click(() => {
+    $(".modal_fade").removeClass("modal_fade_trick");
+    $("#modal_edit_"+number).css("display", "none");
+  });
+
+  $("#modal_cancel_"+number).click(() => {
+    $(".modal_fade").removeClass("modal_fade_trick");
+    $("#modal_edit_"+number).css("display", "none");
+  });
+
+
+
+  // $('#delete_'+number).click(() => {
+  //   $('.modal_fade').addClass('modal_fade_trick');
+  //   $('#modal_'+number).css('display', 'block');
+  // });
+  
+
+  
 });
