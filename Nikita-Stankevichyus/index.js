79ca0da5b193ffa9f84a094a4de03a1f77b6faa3
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-17 21:45:19 +0300

    Modal windows are done. Proceeding to data managing.

--- Nikita-Stankevichyus/index.js
+++ Nikita-Stankevichyus/index.js
@@ -2,25 +2,30 @@
 
 const LIST = [];
 
-function Good(name='', email='', count=0, price=0, russia=[], belorus=[], usa=[]) {
+function Good(name='', email='', count=0, price=0, delivery={}) {
 
   this.name = name;
   this.email = email;
   this.count = count;
   this.price = price;
 
-  this.delivery = {
-    russia: russia,
-    belorus: belorus,
-    usa: usa,
-  }
+  this.delivery = delivery;
 }
 
-
-LIST.push(new Good('Just Good', 'someemail@gmail.com', 3, 222, [true], [], [true,,true]));
-LIST.push(new Good('G Good', 'someeail@gmail.com', 4, 22, [true], [], [true,,true]));
-LIST.push(new Good('B Good', 'smeemail@gmail.com', 5, 222, [], [], [true,,true]));
-LIST.push(new Good('A Good', 'somemail@gmail.com', 1, 222, [true], [], [,,true]));
+LIST.push(new Good('Just Good', 'someemail@gmail.com', 3, 222, {
+  russia: ['Москва', 'Саратов', 'Санкт-Петербург'],
+  belorus: ['Минск', 'Хотляны', 'Бобруйск'],
+  usa: ['Нью-Йорк', 'Вашингтон', 'Бостон']
+}));
+LIST.push(new Good('G Good', 'someeail@gmail.com', 4, 22, {
+  russia: ['Москва', 'Санкт-Петербург'],
+  belorus: ['Минск'],
+}));
+LIST.push(new Good('B Good', 'smeemail@gmail.com',11, 100500, {
+}));
+LIST.push(new Good('A Good', 'somemail@gmail.com', 1, Infinity, {
+  belorus: ['Хотляны'],
+}));
 
 let buttonDelete = $(".button_delete");
 let buttonClose = $(".modal_close");
@@ -35,42 +40,9 @@ $("#triangle_price").click(() => {
 
 
 
-$("#delete_1").click(() => {
-  $(".modal_fade").addClass("modal_fade_trick");
-  $(".my_modal_delete").css("display", "block");
-});
-
-$("#edit_1").click(() => {
-  $(".modal_fade").addClass("modal_fade_trick");
-  $(".my_modal_edit").css("display", "block");
-});
-
-
-$(".modal_close").click(() => {
-  $(".modal_fade").removeClass("modal_fade_trick");
-  $(".my_modal_delete").css("display", "none");
-});
-
-$(".modal_cancel").click(() => {
-  $(".modal_fade").removeClass("modal_fade_trick");
-  $(".my_modal_edit").css("display", "none");
-});
-
 
 let rowTemp = _.template($("#row_template").html());
 
-
-
-
-$("#delete_4").click(() => {
-  $(".modal_fade").addClass("modal_fade_trick");
-  $(".my_modal_delete").css("display", "block");
-});
-
-$("#edit_4").click(() => {
-  $(".modal_fade").addClass("modal_fade_trick");
-  $(".my_modal_edit").css("display", "block");
-});
 
 LIST.forEach((good, number) => {
   $(rowTemp({
@@ -83,20 +55,33 @@ LIST.forEach((good, number) => {
 
   let modalTemp = _.template($("#modal_edit_template").html());
 
-
-
   $(modalTemp({
     modalId: 'modal_edit_'+number,
     name: good.name,
     price: good.price,
     count: good.count,
     email: good.email,
-    city_1: 'FIXME',
-    city_2: 'FIXME',
-    city_3: 'FIXME',
+    city_1: good.delivery.russia ? good.delivery.russia[0] ? good.delivery.russia[0] : null : null,
+    city_2: good.delivery.russia ? good.delivery.russia[1] ? good.delivery.russia[1] : null : null,
+    city_3: good.delivery.russia ? good.delivery.russia[2] ? good.delivery.russia[2] : null : null,
     saveId: 'modal_save_'+number,
     cancelId: 'modal_cancel_'+number,
   })).appendTo('.main');
+
+  const cityCleaner = () => {
+    
+    $('#modal_edit_'+number+' .label_city span').each(
+      (index, element) => {
+        
+        if($(element).html()===''){
+          $(element).parent().empty();
+        }
+      } 
+    );
+
+  }
+
+  cityCleaner();
 
   $('#edit_'+number).click(() => {
     $(".modal_fade").addClass("modal_fade_trick");
@@ -113,13 +98,85 @@ LIST.forEach((good, number) => {
     $("#modal_edit_"+number).css("display", "none");
   });
 
-
-
-  // $('#delete_'+number).click(() => {
-  //   $('.modal_fade').addClass('modal_fade_trick');
-  //   $('#modal_'+number).css('display', 'block');
-  // });
   
 
+  $('#modal_edit_'+number+' .russia').click(() => {
+
+    $('#modal_edit_'+number+' .cities').empty();
+
+    let citiesTemp = _.template($('#edit_cities_template').html());
+
+    $('#modal_edit_'+number+' .cities').html(
+      citiesTemp({
+        city_1: good.delivery.russia ? good.delivery.russia[0] ? good.delivery.russia[0] : '' : '',
+        city_2: good.delivery.russia ? good.delivery.russia[1] ? good.delivery.russia[1] : '' : '',
+        city_3: good.delivery.russia ? good.delivery.russia[2] ? good.delivery.russia[2] : '' : '',
+      })
+    );
+
+    cityCleaner();
+
+  }); 
+
+
+  $('#modal_edit_'+number+' .belorus').click(() => {
+
+    $('#modal_edit_'+number+' .cities').empty();
+
+    let citiesTemp = _.template($('#edit_cities_template').html());
+
+    $('#modal_edit_'+number+' .cities').html(
+      citiesTemp({
+        city_1: good.delivery.belorus ? good.delivery.belorus[0] ? good.delivery.belorus[0] : '' : '',
+        city_2: good.delivery.belorus ? good.delivery.belorus[1] ? good.delivery.belorus[1] : '' : '',
+        city_3: good.delivery.belorus ? good.delivery.belorus[2] ? good.delivery.belorus[2] : '' : '',
+      })
+    );
+
+    cityCleaner();
+
+  });
+
+  
+  $('#modal_edit_'+number+' .usa').click(() => {
+
+    $('#modal_edit_'+number+' .cities').empty();
+
+    let citiesTemp = _.template($('#edit_cities_template').html());
+
+    $('#modal_edit_'+number+' .cities').html(
+      citiesTemp({
+        city_1: good.delivery.usa ? good.delivery.usa[0] ? good.delivery.usa[0] : '' : '',
+        city_2: good.delivery.usa ? good.delivery.usa[1] ? good.delivery.usa[1] : '' : '',
+        city_3: good.delivery.usa ? good.delivery.usa[2] ? good.delivery.usa[2] : '' : '',
+      })
+    );
+
+    cityCleaner();
+
+  });
+
+  let deleteTemp = _.template($('#modal_delete_template').html());
+
+  $(deleteTemp({
+    deleteId: 'modal_delete_'+number,
+    idYes: 'yes_'+number,
+    idNo: 'no_'+number,
+  })).appendTo('.main');
+
+  $('#delete_'+number).click(() => {
+    $('.modal_fade').addClass('modal_fade_trick');
+    $('#modal_delete_'+number).css('display', 'block');
+  });
+  
+  $('#yes_'+number).click(() => {
+    $('.modal_fade').removeClass('modal_fade_trick');
+    $('#modal_delete_'+number).css('display', 'none');
+  }); 
+
+  $('#no_'+number).click(() => {
+    $('.modal_fade').removeClass('modal_fade_trick');
+    $('#modal_delete_'+number).css('display', 'none');
+  }); 
   
 });
