19d6ad2133ea0a63aba5fa8440b0d3581c443c31
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-22 20:41:51 +0300

    Now there are placeholders. Also browserify is installed.

--- Nikita-Stankevichyus/bundle.js
+++ Nikita-Stankevichyus/bundle.js
@@ -1,3 +1,56 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+
+module.exports = function Good(name='', email='', count=0, price=0, russia=[], belorus=[], usa=[]) {
+
+  this.name = name;
+  this.email = email;
+  this.count = count;
+  this.price = price;
+  this.hidden = false;
+    
+  this.description = loremIpsum;
+
+  // NOTE: Maybe there is a better way of dealing with delivery data storage
+  this.delivery = {
+    russia: {
+      moscow: russia ? russia[0] ? true : false : false,
+      saratov: russia ? russia[1] ? true : false : false,
+      spb: russia ? russia[2] ? true : false : false,
+    },
+    belorus: {
+      minsk: belorus ? belorus[0] ? true : false : false,
+      hotlany: belorus ? belorus[1] ? true : false : false,
+      bobruysk: belorus ? belorus[2] ? true : false : false,
+    },
+    usa: {
+      ny: usa ? usa[0] ? true : false : false,
+      washington: usa ? usa[1] ? true : false : false,
+      boston: usa ? usa[2] ? true : false : false,
+    },
+  };
+
+  this.deliveryToArray = () => {
+    let resultArray = [];
+    for(let country in this.delivery){
+      for(let city in this.delivery[country]){
+        resultArray.push(this.delivery[country][city]);
+      }
+    }
+    return resultArray;
+  }
+
+  this.allCities = (country) => {
+    let toReturn = true;
+    for(let city in this.delivery[country]){
+      if(!this.delivery.russia[city]){
+        toReturn = false;
+      }
+    }
+    return toReturn;
+  };
+}
+},{}],2:[function(require,module,exports){
 "use strict";
 
 const Good = require('./goods.js');
@@ -869,3 +922,5 @@ initPlaceholders('#modal_add');
 
 LIST.render();
 
+
+},{"./goods.js":1}]},{},[2]);
--- Nikita-Stankevichyus/goods.js
+++ Nikita-Stankevichyus/goods.js
@@ -1,10 +1,16 @@
-export function Good(name='', email='', count=0, price=0, russia=[], belorus=[], usa=[]) {
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+
+module.exports = function Good(name='', email='', count=0, price=0, russia=[], belorus=[], usa=[]) {
 
   this.name = name;
   this.email = email;
   this.count = count;
   this.price = price;
+  this.hidden = false;
+    
+  this.description = loremIpsum;
 
+  // NOTE: Maybe there is a better way of dealing with delivery data storage
   this.delivery = {
     russia: {
       moscow: russia ? russia[0] ? true : false : false,
@@ -23,30 +29,20 @@ export function Good(name='', email='', count=0, price=0, russia=[], belorus=[],
     },
   };
 
-  this.russiaAllCities = () => {
-    let toReturn = true;
-    for(let city in this.delivery.russia){
-      if(!city){
-        toReturn = false;
+  this.deliveryToArray = () => {
+    let resultArray = [];
+    for(let country in this.delivery){
+      for(let city in this.delivery[country]){
+        resultArray.push(this.delivery[country][city]);
       }
     }
-    return toReturn;
-  };
+    return resultArray;
+  }
 
-  this.belorusAllCities = () => {
+  this.allCities = (country) => {
     let toReturn = true;
-    for(let city in this.delivery.belorus){
-      if(!city){
-        toReturn = false;
-      }
-    }
-    return toReturn;
-  };
-
-  this.usaAllCities = () => {
-    let toReturn = true;
-    for(let city in this.delivery.usa){
-      if(!city){
+    for(let city in this.delivery[country]){
+      if(!this.delivery.russia[city]){
         toReturn = false;
       }
     }
